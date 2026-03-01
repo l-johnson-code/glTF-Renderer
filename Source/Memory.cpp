eeca4e970ef8be097147a5e6b774d58ed16dda3b
Lewis Johnson <88057028+l-johnson-code@users.noreply.github.com>
2026-03-01 15:29:33 +0000

    Moved functions out of header file to reduce compile times.

--- Source/Memory.cpp
+++ Source/Memory.cpp
@@ -1,5 +1,24 @@
 #include "Memory.h"
 
+#include "Profiling.h"
+
+void* Allocate(size_t size)
+{
+	ProfileZoneScoped();
+	void* ptr = malloc(size);
+	if (ptr) {
+		ProfileAlloc(ptr, size);
+	}
+	return ptr;
+}
+
+void Free(void* ptr)
+{
+	ProfileZoneScoped();
+	ProfileFree(ptr);
+	free(ptr);
+}
+
 #ifdef PROFILING_OVERLOAD_NEW_AND_DELETE
 // Overwrite new and delete operators so that we can profile allocations.
 void* operator new(size_t size)
@@ -12,3 +31,51 @@ void operator delete(void* ptr) noexcept
     Free(ptr);
 }
 #endif
+
+void Copy(void* destination, void* source, size_t size)
+{
+	ProfileZoneScoped();
+	memcpy(destination, source, size);
+}
+
+void Copy(void* destination, void* source, size_t element_size, uint32_t element_count, size_t source_stride)
+{
+	ProfileZoneScoped();
+	if (element_size == source_stride) {
+		memcpy(destination, source, element_size * element_count);
+	} else {
+		for (uint32_t i = 0; i < element_count; i++) {
+			std::byte* destination_bytes = (std::byte*)destination;
+			std::byte* source_bytes = (std::byte*)source;
+			memcpy(destination_bytes + i * element_size, source_bytes + i * source_stride, element_size);
+		}
+	}
+}
+
+int CalculateGroupedAllocationSize(Allocation* allocations, int allocation_count)
+{
+	int required_size = 0;
+	for (int i = 0; i < allocation_count; i++) {
+		required_size = Align(required_size, allocations[i].alignment);
+		required_size += allocations[i].size;
+	}
+	return required_size;
+}
+
+int CalculateGroupedAllocationSizeAndOffsets(Allocation* allocations, int allocation_count, int* offsets)
+{
+	int required_size = 0;
+	for (int i = 0; i < allocation_count; i++) {
+		required_size = Align(required_size, allocations[i].alignment);
+		offsets[i] = required_size;
+		required_size += allocations[i].size;
+	}
+	return required_size;
+}
+
+void ApplyGroupedAllocationOffsets(int* offsets, int offset_count, void* base, void** out_pointers)
+{
+	for (int i = 0; i < offset_count; i++) {
+		out_pointers[i] = (std::byte*)base + offsets[i];
+	}
+}
--- Source/Memory.h
+++ Source/Memory.h
@@ -1,28 +1,13 @@
 #pragma once
 
 #include <cassert>
-#include <cstddef>
 #include <cstdint>
-#include <cstring>
+#include <cstddef>
 
-#include "Profiling.h"
-
-inline void* Allocate(size_t size)
-{
-	ProfileZoneScoped();
-	void* ptr = malloc(size);
-	if (ptr) {
-		ProfileAlloc(ptr, size);
-	}
-	return ptr;
-}
-
-inline void Free(void* ptr)
-{
-	ProfileZoneScoped();
-	ProfileFree(ptr);
-	free(ptr);
-}
+struct Allocation {
+	int size;
+	int alignment;
+};
 
 constexpr size_t Mebibytes(size_t mebibytes)
 {
@@ -55,55 +40,10 @@ constexpr size_t Align(size_t offset, size_t alignment)
 	return alignment == 0 ? offset : alignment * ((offset + alignment - 1) / alignment);
 }
 
-inline void Copy(void* destination, void* source, size_t size)
-{
-	ProfileZoneScoped();
-	memcpy(destination, source, size);
-}
-
-inline void Copy(void* destination, void* source, size_t element_size, uint32_t element_count, size_t source_stride)
-{
-	ProfileZoneScoped();
-	if (element_size == source_stride) {
-		memcpy(destination, source, element_size * element_count);
-	} else {
-		for (uint32_t i = 0; i < element_count; i++) {
-			std::byte* destination_bytes = (std::byte*)destination;
-			std::byte* source_bytes = (std::byte*)source;
-			memcpy(destination_bytes + i * element_size, source_bytes + i * source_stride, element_size);
-		}
-	}
-}
-
-struct Allocation {
-	int size;
-	int alignment;
-};
-
-inline int CalculateGroupedAllocationSize(Allocation* allocations, int allocation_count)
-{
-	int required_size = 0;
-	for (int i = 0; i < allocation_count; i++) {
-		required_size = Align(required_size, allocations[i].alignment);
-		required_size += allocations[i].size;
-	}
-	return required_size;
-}
-
-inline int CalculateGroupedAllocationSizeAndOffsets(Allocation* allocations, int allocation_count, int* offsets)
-{
-	int required_size = 0;
-	for (int i = 0; i < allocation_count; i++) {
-		required_size = Align(required_size, allocations[i].alignment);
-		offsets[i] = required_size;
-		required_size += allocations[i].size;
-	}
-	return required_size;
-}
-
-inline void ApplyGroupedAllocationOffsets(int* offsets, int offset_count, void* base, void** out_pointers)
-{
-	for (int i = 0; i < offset_count; i++) {
-		out_pointers[i] = (std::byte*)base + offsets[i];
-	}
-}
+void* Allocate(size_t size);
+void Free(void* ptr);
+void Copy(void* destination, void* source, size_t size);
+void Copy(void* destination, void* source, size_t element_size, uint32_t element_count, size_t source_stride);
+int CalculateGroupedAllocationSize(Allocation* allocations, int allocation_count);
+int CalculateGroupedAllocationSizeAndOffsets(Allocation* allocations, int allocation_count, int* offsets);
+void ApplyGroupedAllocationOffsets(int* offsets, int offset_count, void* base, void** out_pointers);
